dcd9355ddba82052d9d40d203819da7689879e28
phuong <thephuong3011@gmail.com>
2023-10-25 13:28:20 +0700

    update user's cart

---
+++ app/(routes)/cart/components/summary.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import axios from "axios";
+import { useEffect } from "react";
+import { useSearchParams } from "next/navigation";
+
+import Button from "@/components/ui/button";
+import Currency from "@/components/ui/currency";
+import useCart from "@/hooks/use-cart";
+import { toast } from "react-hot-toast";
+
+const Summary = () => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default Summary;
